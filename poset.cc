4893b2830b24005ee166f5444cb0ce9849f84495
Quendora <quendora@gmail.com>
2019-10-21 18:54:21 +0200

    Structures

--- poset.cc
+++ poset.cc
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+// tymczasowe, zeby przejrzysciej sie pisalo
+using namespace std;
+
+using Id = unsigned int;
+using Element = string;
+using Relations = unordered_set<Element>;
+using GreaterThanRelations = Relations;
+using LessThanRelations = Relations;
+using ElementRelations = pair<GreaterThanRelations, LessThanRelations>;
+using Poset = unordered_map<Element, ElementRelations>;
+using Posets = unordered_map<Id, Poset>;
 
 int main()
 {
